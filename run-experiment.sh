317cfbf2c74467a6f785f29b38edb6bd4cb4faf5
Filip Krikava <krikava@gmail.com>
2018-05-10 12:15:41 +0200

    Updated after all runs

--- run-experiment.sh
+++ run-experiment.sh
@@ -5,4 +5,4 @@ docker exec \
        -u rstudio \
        -w /home/rstudio \
        issta18-genthat \
-       parallel -j4 -a packages.txt --bar --joblog experiment.log --timeout 300 ./extract-tests.R
+       parallel -j4 -a packages.txt --bar --joblog experiment.log --timeout 600 ./extract-tests.R
